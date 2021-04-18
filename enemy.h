8fc6a530eb1fad9dfd887e0b946bc2e724bb735a
Morgan Moore <27morganmoore@gmail.com>
2021-04-18 11:30:35 +0100

    first commit

---
+++ enemy.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "entity.h"
+
+class Enemy
+{
+public:
+	int size = 15;
+	Colour colour = { 255,0,0 };
+	Position pos = { 0,0 };
+	Velocity vel = { 0.0f, 0.0f };
+	
+/*
+	Enemy(int size1, Position pos1, Velocity vel1, Colour colour1)
+	{
+		size = size1;
+		pos = pos1;
+		vel = vel1;
+		colour = colour1;
+	}
+
+*/
+
+
+
+};
+
+
+
